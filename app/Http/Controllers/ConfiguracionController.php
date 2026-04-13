a8ea3631f7b8852156d0c5fba77359ef7dad8c6c
Luis2-d <luisfernandozelada.37@gmail.com>
2026-04-13 00:59:44 -0400

    se agrego el proyecto

---
+++ app/Http/Controllers/ConfiguracionController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Configuracion;
+use Illuminate\Http\Request;
+
+class ConfiguracionController extends Controller
+{
+    public function index()
+    {
+        $jsonData = file_get_contents('http://api.hilariweb.com/divisas');
+        $divisas = json_decode($jsonData, true);
+        $configuracion = Configuracion::first();
+        return view('admin.configuracion.index', compact('configuracion', 'divisas'));
+    }
+
+    public function store(Request $request)
+    {
+        //$datos = $request->all();
+        //return response()->json($datos);
+        request()->validate([
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'direccion' => 'required',
+            'telefono' => 'required',
+            'divisa' => 'required',
+            'correo_electronico' => 'required|email',
+            'logo' => 'image|mimes:jpeg,png,jpg',
+        ]);
+        //buscar si existe la configuración
+        $configuracion = Configuracion::first();
+        if ($configuracion) {
+            $configuracion->nombre = $request->nombre;
+            $configuracion->descripcion = $request->descripcion;
+            $configuracion->direccion = $request->direccion;
+            $configuracion->telefono = $request->telefono;
+            $configuracion->divisa = $request->divisa;
+            $configuracion->web = $request->web;
+            $configuracion->correo_electronico = $request->correo_electronico;
+
+            if ($request->hasFile('logo')) {
+                //eliminar el logo anterior
+                if ($configuracion->logo) {
+                    unlink(public_path($configuracion->logo));
+                }
+                $logoPath = $request->file('logo');
+                $nombreArchivo = time() . '_' . $logoPath->getClientOriginalName();
+                $rutaDestino = public_path('uploads/logos');
+                $logoPath->move($rutaDestino, $nombreArchivo);
+                $configuracion->logo = '/uploads/logos/' . $nombreArchivo;
+            }
+
+            $configuracion->save();
+            return redirect()->route('admin.configuracion.index')
+            ->with('mensaje', 'Configuración actualizada correctamente')
+            ->with('icono', 'success');
+            } else {
+            //crear una nueva configuración
+            $configuracion = new Configuracion();
+            $configuracion->nombre = $request->nombre;
+            $configuracion->descripcion = $request->descripcion;
+            $configuracion->direccion = $request->direccion;
+            $configuracion->telefono = $request->telefono;
+            $configuracion->divisa = $request->divisa;
+            $configuracion->web = $request->web;
+            $configuracion->correo_electronico = $request->correo_electronico;
+
+            if ($request->hasFile('logo')) {
+                $logoPath = $request->file('logo');
+                $nombreArchivo = time() . '_' . $logoPath->getClientOriginalName();
+                $rutaDestino = public_path('uploads/logos');
+                $logoPath->move($rutaDestino, $nombreArchivo);
+                $configuracion->logo = '/uploads/logos/' . $nombreArchivo;
+            }
+
+            $configuracion->save();
+            return redirect()->route('admin.configuracion.index')
+            ->with('mensaje', 'Configuración creada correctamente')
+            ->with('icono', 'success');
+
+        }
+    }
+}
